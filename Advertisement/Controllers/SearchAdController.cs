2a7ccac63aab4713a23d42a595c364b1a7d0eab3
Marsel622 <mars.latinos@gmail.com>
2025-08-18 16:18:49 +0300

    feat: add a method for getting ad platforms
    Closed: #9

---
+++ Advertisement/Controllers/SearchAdController.cs
@@ -0,0 +1,36 @@
+﻿using Advertisement.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Advertisement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchAdController : ControllerBase
+    {
+        private readonly IFileImportData _fileImportData;
+
+        public SearchAdController(IFileImportData fileImportData)
+        {
+            _fileImportData = fileImportData;
+        }
+
+        [HttpGet("Ad")]
+        public async Task <IActionResult> GetAd(string Ad)
+        {
+            List<string> fitsplatform = new List<string>();
+            foreach(var platform in _fileImportData.ImportedData)
+            {
+                foreach (var ads in platform.Value) 
+                {
+                    if (Ad.Contains(ads))
+                    {
+                        fitsplatform.Add(platform.Key);
+                    }
+                }
+            }
+
+            return Ok(fitsplatform);
+        }
+    }
+}
