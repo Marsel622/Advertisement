32940caa3088823a294ce7720a2725673894ec74
Marsel622 <mars.latinos@gmail.com>
2025-08-18 14:03:25 +0300

    feat: add a method for loading ad platforms from a file
    Closed: #4

---
+++ Advertisement/Services/IFileImportData.cs
@@ -0,0 +1,12 @@
+﻿namespace Advertisement.Services
+{
+    public interface IFileImportData
+    {
+        Dictionary<string, List<string>> ImportedData { get; set; }
+    }
+
+    public class FileImportData: IFileImportData
+    {
+        public Dictionary<string, List<string>> ImportedData { get; set; } = new();
+    }
+}
